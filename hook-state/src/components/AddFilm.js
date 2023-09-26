18fac710a2e9a0ef265aad74ae4c89ed50f086b8
Taharka2000 <sammbasow1999@gmail.com>
2023-09-26 12:55:25 +0000

    checkpoint-hook

---
+++ hook-state/src/components/AddFilm.js
@@ -0,0 +1,8 @@
+const Film=[
+    {
+        titre: '',
+        note: '',
+        poste: '', // Champ pour l'URL de l'image
+    }
+]
+export default Film;
---
+++ hook-state/src/components/FilmCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+const FilmCard = ({titre,description,poste,raking}) => {
+  
+  return (
+    <div class="relative flex w-[80%] ml-[70px] p-2 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div class="relative mx-4 mt-4 h-96 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+    <img
+      src={poste}
+      class="h-[100%] w-[100%]"
+    />
+  </div>
+  <div class="p-6">
+    <div class="mb-2 flex items-center justify-between">
+      <h1 class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+        Title:{titre}
+      </h1>
+      <p class="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+       Raking:{raking}
+      </p>
+    </div>
+    <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+      {description}
+    </p>
+  </div>
+  </div>
+    
+  );
+};
+
+export default FilmCard;
